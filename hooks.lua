668c6cd4c4c5e006d944e3ffeb59c5cf25f7d4b0
sontyp <n0citizen@protonmail.com>
2017-04-13 15:15:00 +0200

    Update hooks.lua Alex
    Vorschläge für Umformulierungen der englischen Meldungen. Übersetzungen ins Deutsche.

--- hooks.lua
+++ hooks.lua
@@ -2,6 +2,10 @@ function OnPlayerUsingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, 
     local getAccess = PlayerHasBlockAccess(Player, BlockX, BlockY, BlockZ)
     if getAccess == false then
         Player:SendMessage(cChatColor.Rose .. "You do not have permission to open this")
+                                         --[[ "You have no permission to open this" or
+                                              "You're not permitted to open this" or
+                                              "You're not allowed to open this" ]]
+                                         --[[ "Erlaubnis zum Öffnen verweigert" ]]
         return true
     end
 end
@@ -15,6 +19,10 @@ function OnPlayerBreakingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, Cursor
     end
     if getAccess == false then
         Player:SendMessage(cChatColor.Rose .. "You do not have permission to destroy this");
+                                         --[[ "You have no permission to destroy this" or
+                                              "You're not permitted to destroy this" or
+                                              "You're not allowed to destroy this" ]]
+                                         --[[ "Erlaubnis zum Zerstören verweigert" ]]
         return true;
     end
 end
@@ -23,11 +31,15 @@ function OnUpdatingSign(World, BlockX, BlockY, BlockZ, Line1, Line2, Line3, Line
     if Line1 == "[P]" or Line1 == "[Private]" then
         if not Player:HasPermission("SignLock.Create") then
             Player:SendMessage(cChatColor.Rose .. "You are not Allowed to Protect Chests")
+                                             --[[ "You're not allowed to protect chests" or
+                                                  "You're not permitted to protect chests" ]]
+                                             --[[ "Erlaubnis zum schützen von Kisten verweigert" ]]            
             return false
         end
         if CheckBlock(World:GetBlock(BlockX + 1, BlockY, BlockZ)) then
             if CheckProtect(BlockX + 1, BlockY, BlockZ, World) then
                 Player:SendMessage(cChatColor.Green .. "This block is already protected")
+                                                  --[[ "Dieser Block ist bereits geschützt" ]]                 
                 return false, "[?]", "Already", "Protected"
             end
             SetSide(BlockX, BlockY, BlockZ, "+X", World)
@@ -35,6 +47,7 @@ function OnUpdatingSign(World, BlockX, BlockY, BlockZ, Line1, Line2, Line3, Line
         if CheckBlock(World:GetBlock(BlockX - 1, BlockY, BlockZ)) then
             if CheckProtect(BlockX - 1, BlockY, BlockZ, World) then
                 Player:SendMessage(cChatColor.Green .. "This block is already protected")
+                                                  --[[ "Dieser Block ist bereits geschützt" ]]                
                 return false, "[?]", "Already", "Protected"
             end
             SetSide(BlockX, BlockY, BlockZ, "-X", World)
@@ -42,6 +55,7 @@ function OnUpdatingSign(World, BlockX, BlockY, BlockZ, Line1, Line2, Line3, Line
         if CheckBlock(World:GetBlock(BlockX, BlockY, BlockZ + 1)) then
             if CheckProtect(BlockX, BlockY, BlockZ + 1, World) then
                 Player:SendMessage(cChatColor.Green .. "This block is already protected")
+                                                  --[[ "Dieser Block ist bereits geschützt" ]]                 
                 return false, "[?]", "Already", "Protected"
             end
             SetSide(BlockX, BlockY, BlockZ, "+Z", World)
@@ -49,11 +63,13 @@ function OnUpdatingSign(World, BlockX, BlockY, BlockZ, Line1, Line2, Line3, Line
         if CheckBlock(World:GetBlock(BlockX, BlockY, BlockZ - 1)) then
             if CheckProtect(BlockX, BlockY, BlockZ - 1, World) then
                 Player:SendMessage(cChatColor.Green .. "This block is already protected")
+                                                  --[[ "Dieser Block ist bereits geschützt" ]]                 
                 return false, "[?]", "Already", "Protected"
             end
             SetSide(BlockX, BlockY, BlockZ, "-Z", World)
         end
-        Player:SendMessage(cChatColor.Green .. "You successfully protected you're block")
+        Player:SendMessage(cChatColor.Green .. "You successfully protected your block")
+                                          --[[ "Du hast deinen Block erfolgreich geschützt" ]]         
         if Line2 == "" and Line3 == "" and Line4 == "" then
             return false, Line1, Player:GetName(), Line3, Line4
         end
